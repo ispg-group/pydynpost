a30732f3b623cb5abcdadc95dc141784937c87ac
Yorick Lassmann <yorick.lassmann95@gmail.com>
2022-07-14 17:30:47 +0100

    Changed aims population method such that, as for TSH the mean and stanard error of the mean are calculated in a more memory efficient manner.

--- aimsscripts/src/aimspopulation.py
+++ aimsscripts/src/aimspopulation.py
@@ -16,10 +16,10 @@ class statePopulations(object):
         self.psFile = psFile
         self.dirsInCwd = dirsInCwd
 
-    def popInnerLoop(self, tmp_CWD, N_state, state):
+    def popInnerLoop(self, tmp_CWD, nState, nState2, nrSamples, state):
         """ Gather all populations of a given state corresponding 
             to a certain run (in the case of stochastic selection)
-            and append them to the N_state matrix. """                
+            and append them to the nState matrix. """                
         if self.prsr.AIMStype in ["AIMS", "ESSAIMS", "OSSAIMS","AIMSWISS"]:
             for geom in np.arange(1, self.prsr.sampleSize + 1):
                 if geom in self.prsr.dupList:
@@ -34,14 +34,13 @@ class statePopulations(object):
                 time = N_tmp_dat[:,0] 
                 N_tmp_state = N_tmp_dat[:,state]
                 N_interp_state = np.interp(self.prsr.interpTime, time, N_tmp_state)
-                N_state.append(N_interp_state)
+                nState += N_interp_state
+                nState2 += N_interp_state**2
+                nrSamples += 1
         elif self.prsr.AIMStype == "SINGLE":
             for sample in np.arange(1, self.prsr.sampleSize + 1):
                 if sample in self.prsr.dupList:
                     continue
-                #N_tmp = self.psFile.inputFileName(tmp_CWD, "N.dat",
-                #                                  dirType = "rng", 
-                #                                  numStr = str(sample))
                 N_tmp = self.psFile.inputFileName(tmp_CWD, "N.dat",
                                                   dirType = self.prsr.RNGdir, 
                                                   numStr = str(sample))
@@ -49,7 +48,9 @@ class statePopulations(object):
                 time = N_tmp_dat[:,0] 
                 N_tmp_state = N_tmp_dat[:,state]
                 N_interp_state = np.interp(self.prsr.interpTime, time, N_tmp_state)
-                N_state.append(N_interp_state)
+                nState += N_interp_state
+                nState2 += N_interp_state**2
+                nrSamples += 1
         else:
             dirsInTmp = getDirs(tmp_CWD)
             if dirsInTmp.size != 0:
@@ -59,43 +60,61 @@ class statePopulations(object):
                     time = N_tmp_dat[:,0] 
                     N_tmp_state = N_tmp_dat[:,state]
                     N_interp_state = np.interp(self.prsr.interpTime, time, N_tmp_state)
-                    N_state.append(N_interp_state)
+                    nState += N_interp_state
+                    nState2 += N_interp_state**2
+                    nrSamples += 1
             else:
                 fileName = tmp_CWD + "/N.dat"
                 N_tmp_dat = np.genfromtxt(fileName)
                 time = N_tmp_dat[:,0] 
                 N_tmp_state = N_tmp_dat[:,state]
                 N_interp_state = np.interp(self.prsr.interpTime, time, N_tmp_state)
-                N_state.append(N_interp_state)
+                nState += N_interp_state
+                nState2 += N_interp_state**2
+                nrSamples += 1
 
-        return N_state 
+        return nState, nState2, nrSamples
 
     def getPopulation(self):
         """ Function that calculates the mean population of every state
             and its standard error in the full FMS Bundle. """
         for state in np.arange(1,self.prsr.nrStates+1):
-            N_state = []
+            nState = np.zeros(self.prsr.interpTime.size)
+            nState2 = np.zeros(self.prsr.interpTime.size)
+            nrSamples = 0
             if self.dirsInCwd.size != 0:
                 if self.prsr.AIMStype != "AIMS":
                     if self.prsr.AIMStype in ["ESSAIMS", "OSSAIMS","AIMSWISS"]:
                         for i in np.arange(1, self.prsr.nrRNGs + 1):
                             #tmp_CWD = self.CWD + "/rng" + str(i)
                             tmp_CWD = self.CWD + "/" + self.prsr.RNGdir + str(i)
-                            N_state = self.popInnerLoop(tmp_CWD, N_state, state) 
-                        N_samples = len(N_state)
+                            nState, nState2, nrSamples = self.popInnerLoop(
+                                                             tmp_CWD, nState, 
+                                                             nState2, nrSamples,
+                                                             state
+                                                         ) 
                     elif self.prsr.AIMStype == "SINGLE":
-                        N_state = self.popInnerLoop(self.CWD, N_state, state) 
-                        N_samples = len(N_state)
+                        nState, nState2, nrSamples = self.popInnerLoop(
+                                                         self.CWD, nState,
+                                                         nState2, nrSamples,
+                                                         state
+                                                     ) 
                     else:
                         dirsInTmp = getDirs(self.CWD)
                         for DIR in dirsInTmp:
                             tmp_CWD = self.CWD + "/" + DIR 
-                            N_state = self.popInnerLoop(tmp_CWD, N_state, state) 
-                        N_samples = len(N_state)
+                            nState, nState2, nrSamples = self.popInnerLoop(
+                                                             tmp_CWD, nState,
+                                                             nState2, nrSamples,
+                                                             state
+                                                         ) 
                 else:
                     tmp_CWD = self.CWD 
-                    N_state = self.popInnerLoop(tmp_CWD, N_state, state) 
-                    N_samples = (self.prsr.sampleSize - len(self.prsr.dupList))
+                    nState, nState2, nrSamples = self.popInnerLoop(
+                                                    tmp_CWD, nState,
+                                                    nState2, nrSamples,
+                                                    state
+                                                ) 
             else:
                 # In case AIMS population data is only supplied as N.dat files
                 for i in np.arange(1, self.prsr.sampleSize + 1):
@@ -106,29 +125,23 @@ class statePopulations(object):
                     N_tmp_state = N_tmp_dat[:,state]
                     time = N_tmp_dat[:,0] 
                     N_interp_state = np.interp(self.prsr.interpTime, time, N_tmp_state)
-                    N_state.append(N_interp_state)
+                    nState += N_interp_state
+                    nState2 += N_interp_state**2
 
-                N_samples = (self.prsr.sampleSize - len(self.prsr.dupList))
+                nrSamples = (self.prsr.sampleSize - len(self.prsr.dupList))
                         
 
             print("")
-            print("The total number of unique ICs is  \t" + str(N_samples))
+            print("The total number of unique ICs is  \t" + str(nrSamples))
 
-            N_state_cum = np.zeros((self.prsr.interpTime.size,)) 
-            for i in np.arange(N_samples):
-                N_state_cum += N_state[i]
-                
-            N_state_mean =  N_state_cum/N_samples
+            nState_mean = nState / nrSamples
+            nState_std = nState2 / nrSamples - nState_mean**2 
+            nState_std = np.sqrt(1./(nrSamples - 1) * nState_std)
 
-            N_state_stderr = np.zeros((self.prsr.interpTime.size,)) 
-            for i in np.arange(N_samples):
-                N_state_stderr += (N_state[i] - N_state_mean)**2
-
-            N_state_stderr = np.sqrt(N_state_stderr/(N_samples * (N_samples-1)))
-            print("Maximum stderr: \t" + str(np.max(N_state_stderr)))
+            print("Maximum stderr: \t" + str(np.max(nState_std)))
 
             stateStr = str(state)
-            N_state_file = "N_" + stateStr + "_" + self.prsr.AIMStype + ".dat"
-            np.savetxt(N_state_file, np.array([self.prsr.interpTime, N_state_mean,
-                                               N_state_stderr]).T,
+            nState_file = "N_" + stateStr + "_" + self.prsr.AIMStype + ".dat"
+            np.savetxt(nState_file, np.array([self.prsr.interpTime, nState_mean,
+                                               nState_std]).T,
                        fmt="%8d %30.18e %30.18e")
